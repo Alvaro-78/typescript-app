900430545beb2e4b6358fb31d1f5ff861c873b4c
alvaro-78 <alvaro78an@gmail.com>
2021-12-09 08:45:14 +0100

    Timer.tsx useEffect

---
+++ src/components/Timer.tsx
@@ -0,0 +1,31 @@
+import { useState, useEffect, useRef } from 'react';
+
+// creamos un type, para pasarle los argumentos/propiedades del componente hijo al componente padre
+
+type TimerProps = {
+	miliseconds: number;
+};
+
+const Timer = ({ miliseconds }: TimerProps) => {
+	const [seconds, setSeconds] = useState(0);
+	const ref = useRef<NodeJS.Timeout>();
+
+	useEffect(() => {
+		ref.current && clearInterval(ref.current);
+
+		ref.current = setInterval(
+			() => setSeconds((seconds) => seconds + 1),
+			miliseconds
+		);
+	}, [miliseconds]);
+
+	return (
+		<div>
+			<h4>
+				Timer: <small>{seconds}</small>
+			</h4>
+		</div>
+	);
+};
+
+export default Timer;
